0e0931157facbec75cb4ecbb768ca7e98612314d
vishal <vishalks9576@gmail.com>
2026-05-17 14:47:38 +0530

    fix railway mysql

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mysql = require('mysql2/promise');
-
 require('dotenv').config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
+    host: 'interchange.proxy.rlwy.net',
+    user: 'root',
+    password: 'VkhxxHYBJLoQLPyVPrtlzgOLWPwfHLud',
+    database: 'railway',
+    port: 45210,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
