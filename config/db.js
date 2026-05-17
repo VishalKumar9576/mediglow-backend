454c9020ae3b4120e7b9e0f2fc0ff23e28c92acd
vishal <vishalks9576@gmail.com>
2026-05-17 14:39:28 +0530

    update railway db credentials

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2/promise');
+
 require('dotenv').config();
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
